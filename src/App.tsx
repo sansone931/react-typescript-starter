8f67d6f67390e0ff31786329e3e7c1f106d48a03
sansone931 <sansone931@users.noreply.github.com>
2020-10-15 11:12:09 -0300

    Refactor React components
    Add basic directory structure for components and extract components from
    `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
+import { Home } from './pages';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { theme } from './styles/theme';
-
-const Header = styled.h1`
-  color: ${({ theme }) => theme.colors.main};
-`;
 
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
-    <Header>React TypeScript Starter</Header>
+    <Home />
   </ThemeProvider>
 );
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { Header } from '../../components';
+
+export const Home: React.FC = () => <Header>React TypeScript Starter</Header>;
